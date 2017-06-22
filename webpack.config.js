810ca098d83c342dcefe0a7cf7d5de135488a458
mikayeldanielyan <michael@helix.am>
2017-06-22 19:09:27 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const env = process.env.NODE_ENV;
+
+const isDebug = env !== 'production';
+
+const VENDOR_LIBS = [
+  'react',
+  'react-dom',
+  'react-redux',
+  'react-router',
+  'redux',
+];
+
+module.exports = {
+  devtool: isDebug ? '' : 'source-map',
+  entry: {
+    bundle: './src/index.jsx',
+    vendor: VENDOR_LIBS,
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+  },
+
+  resolve: {
+    alias: {
+      'modules/product': path.resolve(__dirname, 'src/modules/product'),
+    },
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: isDebug,
+                importLoaders: 1,
+                localIdentName: isDebug ? '[name]__[local]___[hash:base64:5]' : '[hash:base64:5]',
+                minimize: !isDebug,
+                discardComments: { removeAll: true },
+              },
+            },
+            {
+              loader: 'postcss-loader',
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.(eot|woff|woff2)$/,
+        loader: 'file-loader?name=fonts/[name].[ext]',
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader',
+        query: {
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new ExtractTextPlugin('styles.css'),
+  ],
+
+  devServer: {
+    disableHostCheck: true,
+    host: '0.0.0.0',
+  },
+};
