b79f8f77e7652c760e62a5bfc99b7367186fb814
mikayeldanielyan <michael@helix.am>
2017-07-18 02:31:18 +0300

    added redux store and updated dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,22 @@ import 'babel-polyfill';
 
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
 
-render(<App />, document.getElementById('react-root'));
+import App from './App';
+import reducer from './reducer';
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+
+render(
+  (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ),
+  document.getElementById('react-root'),
+);
+
